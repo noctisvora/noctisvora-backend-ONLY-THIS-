8eaa899091abbddaaabbbb3010452db37f8ba7bc
noctisvora <noctisvora@gmail.com>
2026-05-29 17:14:03 +0530

    server.js

--- server.js
+++ server.js
@@ -15,10 +15,10 @@ const PORT = process.env.PORT || 3000;
 /* ================= DATABASE ================= */
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "root123",
-    database: "noctisvora"
+    host: process.env.DB_HOST
+user: process.env.DB_USER
+password: process.env.DB_PASS
+database: process.env.DB_NAMEra"
 });
 
 /* ================= EMAIL ================= */
